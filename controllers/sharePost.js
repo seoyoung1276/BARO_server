27ffb92438efee316f7dedd06eae53ed2ce00902
이서영 <88700475+seoyoung1276@users.noreply.github.com>
2023-11-13 11:11:01 +0900

    Update .gitignore to ignore migrations

---
+++ controllers/sharePost.js
@@ -0,0 +1,17 @@
+exports.createPost = async (req, res) =>{
+    try {
+        const {user_no, title, content} = req.body;
+        const currentUser = req.user; 
+
+        const newPost = await share_posts.create({
+            user_no: currentUser.user_no,
+            title,
+            content,
+        });
+        
+        res.json(newPost)
+    } catch(error){
+        console.log(error);
+        res.status(500).json({ error : error})
+    }
+}
