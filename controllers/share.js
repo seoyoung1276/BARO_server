06d60b936368e13af881e8212f0b073a38d2fe79
이서영 <88700475+seoyoung1276@users.noreply.github.com>
2023-11-16 04:15:40 +0900

    나누기 글 쓰기, 모든 글 불러오기, 한가지 글 불러오기 구현

--- controllers/share.js
+++ controllers/share.js
@@ -1,20 +1,49 @@
-const { share_posts } = require('../models'); // 모델 가져오기
+const SharePost = require('../models/share_posts'); // 모델 파일 경로에 맞게 변경
 
 // 나누기 글 쓰기
-exports.createPost = async (req, res) =>{
-    try {
-        const {user_no, title, content} = req.body;
-        const currentUser = req.user; 
+exports.createPost = async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    const currentUser = req.user; 
 
-        const newPost = await share_posts.create({
-            user_no: currentUser.user_no,
-            title,
-            content,
-        });
-        
-        res.json(newPost)
-    } catch(error){
+    const newPost = await SharePost.create({
+      user_no: currentUser.user_no,
+      title,
+      content,
+    });
+    res.json(newPost);
+  } catch(error){
+    console.log(error);
+    res.status(500).json({ error : error });
+  }
+};
+
+
+// 나누기의 모든 글 불러오기
+exports.getAllSharePost = async (req, res) => {
+    try {
+    const allSharePost = await SharePost.findAll({
+        order: [['date', 'DESC']] // 최신순으로 정렬..
+    });
+    res.json(allSharePost)
+    }catch (error){
         console.log(error);
         res.status(500).json({ error : error})
+    }
+
+}
+
+// 나누기의 한가지 글 불러오기
+exports.getOneSharePost = async (req, res) =>{
+    try{
+        const postno = req.params.postno
+        const oneSharePost = await SharePost.findOne({
+            where: {post_no: postno}
+        });
+        res.json(oneSharePost)
+
+    }catch (error){
+        console.log(error);
+        res.status(500).json({ error: error})
     }
 }
--- routes/share.js
+++ routes/share.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const { isLoggedin } = require('../middlewares');
 const router = express.Router()
-const {createPost} = require('../controllers/share');
+const { createPost, getAllSharePost, getOneSharePost} = require('../controllers/share');
 
 // 나누기 글쓰기
-router.post('/post',createPost);
+router.post('/post', createPost);
+
+//나누기 글 전체 조회 (나누기 카테고리를 누르면 전부 나와야 한다. 최신글이 위에 어떻게 나오게 해야할까... 아 데이트로 정렬하면될듯!)
+router.get('/post', getAllSharePost);
+
+// 나누기 글 번호로 글 조회하기 (여기가 글 클릭시 나올것!) 
+router.get('/post/:postno', getOneSharePost)
+
+// 게시글 수정과 삭제... 일단 내 글인지 확인 해야함 controller에서 req.user 와 글의 user.no 비교하면 될듯
+
+
 
 module.exports = router;
