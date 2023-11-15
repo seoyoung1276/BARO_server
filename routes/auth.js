9b9056bf21130f4dba828cf8c5af60b69e1be0d1
이서영 <88700475+seoyoung1276@users.noreply.github.com>
2023-11-15 19:40:41 +0900

    코드 정리

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const passport = require('passport');
-const axios = require('axios');
 const { isLoggedin } = require('../middlewares');
 const router = express.Router()
 
@@ -24,25 +23,7 @@ router.get('/logout', isLoggedin, (req, res) =>{
     req.logOut();
     res.redirect('/');
 });
-// router.post('/google', async (req, res) => {
-//     const { accessToken } = req.body;
 
-//     try {
-//         const response = await axios.get(
-//             `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${accessToken}`
-//         );
-
-//         const userData = response.data;
-//         res.status(200).json({ userData });
-//     } catch (error){ 
-//         console.error('Error fetching user data:', error);
-//         res.status(500).json({ error: 'Failed to fetch user data' });
-//     }
-// })
-
-router.get('/', (req, res) => {
-    res.send("되냐?");
-})
 
 
 module.exports = router
