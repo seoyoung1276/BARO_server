993f09ae995f6c799a5595d86ebba8495660fd6b
이서영 <88700475+seoyoung1276@users.noreply.github.com>
2023-11-16 04:15:51 +0900

    pw env 설정

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const config = require('../config/config.json') [process.env.NODE_ENV || 'development']
+const config = require('../config/config.js') [process.env.NODE_ENV || 'development']
 
 const db = {};
 
