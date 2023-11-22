7378f4a2d4cb7d9f338fadeebab6165caaa8d6a2
이서영 <88700475+seoyoung1276@users.noreply.github.com>
2023-11-22 22:51:23 +0900

    로그인 실패시 경로 수정

--- public/js/index.js
+++ public/js/index.js
@@ -4,7 +4,7 @@ function handleGoogleLogin() {
     window.location.href = 'http://ec2-13-125-87-160.ap-northeast-2.compute.amazonaws.com:3000/auth/google/';
 }
 
-axios.get('auth/login-failed')
+axios.get('/auth/google/auth/login-failed')
 .then(response =>{
     alert('회원가입은 미림 학교 계정으로만 가능합니다.');
 })
