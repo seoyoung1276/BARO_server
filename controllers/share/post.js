3b7f28ffeb769fe37e26d3036a82ca759b64feba
이서영 <88700475+seoyoung1276@users.noreply.github.com>
2023-11-22 22:32:10 +0900

    나누기 삭제 수정

--- controllers/share/post.js
+++ controllers/share/post.js
@@ -87,14 +87,12 @@ exports.updatePost = async (req, res) =>{
 exports.deletePost = async (req, res) =>{
   try{
     const postNo = req.params.postno;
-
     const deletePost = await SharePost.destory({
       where: {
         id: postNo
       }
-    }).then(() =>{
-      res.json(deletePost);
     })
+    res.json(deletePost);
 
   }catch(error){
     console.log(error);
