a080cccabd36d0b80508d8f3d235d318a1d87ec9
이서영 <88700475+seoyoung1276@users.noreply.github.com>
2023-11-22 22:40:33 +0900

    혹시?

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,7 @@ function handleGoogleLogin() {
     window.location.href = 'http://ec2-13-125-87-160.ap-northeast-2.compute.amazonaws.com:3000/auth/google/';
 }
 
+/*
 axios.get('/login-failed')
 .then(response =>{
     alert('회원가입은 미림 학교 계정으로만 가능합니다.');
@@ -11,3 +12,4 @@ axios.get('/login-failed')
 .catch(error =>{
     console.error('There has been a problem with your axios request:', error);
 });
+*/
