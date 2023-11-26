3f0538de7cf396358b059f59ed5e8e6ca829f84c
이서영 <88700475+seoyoung1276@users.noreply.github.com>
2023-11-26 21:39:17 +0900

    댓글 작성 및 가져오기 수정

--- controllers/share/comment.js
+++ controllers/share/comment.js
@@ -1,38 +1,43 @@
-const ShareComment = require('../../models/ShareComment'); 
+const ShareComment = require('../../models/ShareComment');
 
 exports.postComment = async (req, res) => {
-    try{
+    try {
         const postId = req.params.postid;
-        const { user_no, post_id, content, responseTo } = req.body;
+        const { user_no, content, responseTo } = req.body;
 
         let response;
-        if(responseTo === undefined) {
+        if (responseTo === undefined) {
             response = await ShareComment.create({
                 user_no: user_no,
-                post_id: post_id,
+                post_id: postId,
                 content: content
             });
-            res.json(response);
-        }else{
-            const response = await ShareComment.create({
+        } else {
+            response = await ShareComment.create({
                 user_no: user_no,
-                post_id: postId,
+                post_id: postId, // postId 변수 이름을 'postId'로 변경하여 일관성 있게 수정
                 content: content,
-                responseto: responseTo 
-            })
-            res.json(response);
+                responseTo: responseTo // 'responseTo'의 대소문자를 올바르게 수정
+            });
         }
-    }catch (error){
-        res.status(500).json({error:error})
+        res.json(response);
+    } catch (error) {
+        res.status(500).json({ error: error });
     }
 };
 
-// exports.getComment = async (req, res) =>{
+exports.getComment = async (req, res) => {
+    try {
+        const postId = req.params.postid;
+        const comments = await ShareComment.find({ post_id: postId }); // 특정 포스트 ID에 대한 댓글 가져오기
+        res.json(comments);
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+};
+
+// exports.patchComment = async (req, res) => {
 //     try{
-//         const postId = req.params.postid;
-//     }catch (error){
-//         res.status(500).json({ error: error })
+
 //     }
 // }
-
-// 댓글 수정
