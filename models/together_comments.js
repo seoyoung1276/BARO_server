51df0be8937b75f2d54c899078798e624ddb42d3
이서영 <88700475+seoyoung1276@users.noreply.github.com>
2023-12-03 22:22:27 +0900

    배우기 댓글 함께하기 댓글 시퀄라이즈 모델 수정

--- models/together_comments.js
+++ models/together_comments.js
@@ -1,68 +1,57 @@
-const Sequelize = require('sequelize');
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('together_comments', {
-    id: {
-      autoIncrement: true,
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true
-    },
-    user_no: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-      references: {
-        model: 'users',
-        key: 'user_no'
+const { Sequelize, DataTypes } = require('sequelize');
+
+class TogetherComment extends Sequelize.Model{
+  static initModel(sequelize) {
+    TogetherComment.initModel({
+      id: {
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true
+      },
+      user_no: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'users',
+          key: 'user_no'
+        }
+      },
+      post_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'together_posts',
+          key: 'id'
+        }
+      },
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: true
+      },
+      date: {
+        type: DataTypes.DATE,
+        allowNull: true,
+        defaultValue: Sequelize.Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      responseto: {
+        type: DataTypes.INTEGER,
+        allowNull: true
       }
-    },
-    post_id: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-      references: {
-        model: 'together_posts',
-        key: 'id'
-      }
-    },
-    content: {
-      type: DataTypes.TEXT,
-      allowNull: true
-    },
-    date: {
-      type: DataTypes.DATE,
-      allowNull: true,
-      defaultValue: Sequelize.Sequelize.literal('CURRENT_TIMESTAMP')
-    },
-    responseto: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    }
-  }, {
-    sequelize,
-    tableName: 'together_comments',
-    timestamps: false,
-    indexes: [
-      {
-        name: "PRIMARY",
-        unique: true,
-        using: "BTREE",
-        fields: [
-          { name: "id" },
-        ]
-      },
-      {
-        name: "user_no",
-        using: "BTREE",
-        fields: [
-          { name: "user_no" },
-        ]
-      },
-      {
-        name: "post_id",
-        using: "BTREE",
-        fields: [
-          { name: "post_id" },
-        ]
-      },
-    ]
-  });
+    },{
+      sequelize,
+      timestamps: false,
+      underscored: false,
+      modelName: 'TogetherComment',
+      tableName: 'together_comments',
+      paranoid: false,
+      charset: 'utf8',
+      collate: 'utf8_general_ci',
+    });
+  } static associate(db) {
+    db.togetherComment.belongTo(db.User, {foreignKey: 'user_no'})
+    db.togetherComment.belongTo(db.TogetherPost, {foreignKey: 'id'})
+  }
 };
+
+module.exports = togetherComment;
