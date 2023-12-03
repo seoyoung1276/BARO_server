3ca90e14ea98accfccc30b0d003a921b83b84cdc
seohyunCho <shanny06@naver.com>
2023-12-03 18:12:38 +0900

    사용자 이름 undefined 오류 해결

--- public/js/shareShowPost.js
+++ public/js/shareShowPost.js
@@ -53,41 +53,7 @@ function showComments(comments){
         console.log(comment);
         getUserName(comment);
         console.log(userName);
-        let parentDiv = document.getElementsByClassName('comment-area')[0];
         
-        let finalDiv = document.createElement('div');
-        finalDiv.className = "comments";
-
-        let profileDiv = document.createElement('div');
-        profileDiv.className = "comment-profile";
-
-        let commentUsername = document.createElement('div');
-        commentUsername.className = "comment-username";
-        commentUsername.innerHTML = userName;
-
-        profileDiv.innerHTML = `<iconify-icon icon="healthicons:ui-user-profile" class="user-comment-profile-img"></iconify-icon>`;
-
-        profileDiv.appendChild(commentUsername);
-
-        let commentContentDiv = document.createElement('div');
-        commentContentDiv.className = "comment-content";
-        commentContentDiv.innerHTML = comment.content;
-
-        let commentInfoDiv = document.createElement('div');
-        commentInfoDiv.className = "comment-info";
-
-        let commentDate = document.createElement('div');
-        commentDate.className = "comment-date";
-        commentDate.innerHTML = comment.date;
-
-        commentInfoDiv.appendChild(commentDate);
-        commentInfoDiv.innerHTML += `<iconify-icon icon="ic:baseline-comment" class="add-comment"></iconify-icon>`;
-    
-        finalDiv.appendChild(profileDiv);
-        finalDiv.appendChild(commentContentDiv);
-        finalDiv.appendChild(commentInfoDiv);
-
-        parentDiv.appendChild(finalDiv);
     }
 
     let sortSubComments = subComments.sort((one, two) => {
@@ -109,9 +75,55 @@ function showComments(comments){
         });
         console.log(commentIndex);
 
-        let userName = getUserName(comment);
-    
-        let finalDiv = document.createElement('div');
+        getSubUserName(comment);
+    }
+    functionOpen();
+}
+
+let subIndex;
+function functionOpen(){
+    let subComment = [...document.getElementsByClassName('add-comment')];
+    subComment.forEach((e, i) => {
+        e.onclick = () => {
+            subIndex = i;
+            addSubComment(i);
+        }
+    })
+}
+
+function addSubComment(index){
+    let comment = document.getElementsByClassName('comment-username')[index];
+    document.getElementsByClassName('input-comment')[0].placeholder = `${comment.innerText}님 에게`;
+}
+
+function getUserName(comment){
+    console.log(comment);
+    axios.get(`${BASE_URL}/user/${comment.user_no}`)
+    .then(Response => {
+        console.log(Response.data.result.name)
+        userName = Response.data.result.name;
+        makeComments(comment, Response.data.result.name);
+    })
+    .catch(error => {
+        console.error('There has been a problem with your axios request:', error);
+    });
+}
+
+function getSubUserName(comment){
+    console.log(comment);
+    axios.get(`${BASE_URL}/user/${comment.user_no}`)
+    .then(Response => {
+        console.log(Response.data.result.name)
+        userName = Response.data.result.name;
+        makeSubComments(comment, Response.data.result.name);
+    })
+    .catch(error => {
+        console.error('There has been a problem with your axios request:', error);
+    });
+}
+
+function makeSubComments(comment, userName){
+    let finalDiv = document.createElement('div');
         finalDiv.className = "sub-comments";
     
         let commentDetailDiv = document.createElement('div');
@@ -149,36 +161,44 @@ function showComments(comments){
         finalDiv.appendChild(commentDetailDiv);
 
         document.getElementsByClassName('comments')[commentIndex].after(finalDiv);
-    }
-    functionOpen();
 }
 
-let subIndex;
-function functionOpen(){
-    let subComment = [...document.getElementsByClassName('add-comment')];
-    subComment.forEach((e, i) => {
-        e.onclick = () => {
-            subIndex = i;
-            addSubComment(i);
-        }
-    })
-}
+function makeComments(comment, userName){
+    let parentDiv = document.getElementsByClassName('comment-area')[0];
+        
+    let finalDiv = document.createElement('div');
+    finalDiv.className = "comments";
 
-function addSubComment(index){
-    let comment = document.getElementsByClassName('comment-username')[index];
-    document.getElementsByClassName('input-comment')[0].placeholder = `${comment.innerText}님 에게`;
-}
+    let profileDiv = document.createElement('div');
+    profileDiv.className = "comment-profile";
 
-function getUserName(comment){
-    console.log(comment);
-    axios.get(`${BASE_URL}/user/${comment.user_no}`)
-    .then(Response => {
-        console.log(Response.data.result.name)
-        userName = Response.data.result.name;
-    })
-    .catch(error => {
-        console.error('There has been a problem with your axios request:', error);
-    });
+    let commentUsername = document.createElement('div');
+    commentUsername.className = "comment-username";
+    commentUsername.innerHTML = userName;
+
+    profileDiv.innerHTML = `<iconify-icon icon="healthicons:ui-user-profile" class="user-comment-profile-img"></iconify-icon>`;
+
+    profileDiv.appendChild(commentUsername);
+
+    let commentContentDiv = document.createElement('div');
+    commentContentDiv.className = "comment-content";
+    commentContentDiv.innerHTML = comment.content;
+
+    let commentInfoDiv = document.createElement('div');
+    commentInfoDiv.className = "comment-info";
+
+    let commentDate = document.createElement('div');
+    commentDate.className = "comment-date";
+    commentDate.innerHTML = comment.date;
+
+    commentInfoDiv.appendChild(commentDate);
+    commentInfoDiv.innerHTML += `<iconify-icon icon="ic:baseline-comment" class="add-comment"></iconify-icon>`;
+
+    finalDiv.appendChild(profileDiv);
+    finalDiv.appendChild(commentContentDiv);
+    finalDiv.appendChild(commentInfoDiv);
+
+    parentDiv.appendChild(finalDiv);
 }
 
 function backBtn(){
