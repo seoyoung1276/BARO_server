b2314c7d8c313b1e9daecaced78a44c43775c7ee
seohyunCho <shanny06@naver.com>
2023-12-04 03:33:25 +0900

    나의 등급 화면 구성

--- public/js/myLevel.js
+++ public/js/myLevel.js
@@ -22,7 +22,73 @@ const levelContent = [
 
 ]
 
+let commentSum = 0;
 
+getInfo();
+async function getInfo(){
+    const userno = await getUserNo();
+    // router.get('/:userno', getUserComments)
+    axios.get(`${BASE_URL}/together/comment/user/${userno}`)
+    .then(Response => {
+        console.log(Response.data);
+        commentSum += Response.data.length;
+        getShareCommentLength(userno);
+    })
+    .catch(error => {
+        console.error('There has been a problem with your axios request:', error);
+    });
+}
+
+function getShareCommentLength(userno){
+    axios.get(`${BASE_URL}/share/comment/user/${userno}`)
+    .then(Response => {
+        console.log(Response.data);
+        commentSum += Response.data.length;
+        getLearnCommentLength(userno);
+    })
+    .catch(error => {
+        console.error('There has been a problem with your axios request:', error);
+    });
+}
+function getLearnCommentLength(userno){
+    axios.get(`${BASE_URL}/learn/comment/user/${userno}`)
+    .then(Response => {
+        console.log(Response.data);
+        commentSum += Response.data.length;
+        getCurrectLevel(commentSum);
+    })
+    .catch(error => {
+        console.error('There has been a problem with your axios request:', error);
+    });
+}
+function getCurrectLevel(sum){
+    let icon = document.getElementsByClassName('my-level-stemp')[0];
+    let name = document.getElementsByClassName('my-level')[0];
+    
+    let remain = 0;
+    if(sum < 5) {
+        icon.src = "/img/Lv1_stemp.png";
+        name.innerText = "현재 등급은 Lv.1 거북이입니다"
+        remain = 5 - sum;
+    }else if(sum < 15){
+        icon.src = "/img/Lv2_stemp.png";
+        name.innerText = "현재 등급은 Lv.2 양입니다"
+        remain = 15 - sum;
+    }else if(sum < 30) {
+        icon.src = "/img/Lv3_stemp.png";
+        name.innerText = "현재 등급은 Lv.3 강아지입니다"
+        remain = 30 - sum;
+    }else if(sum < 50) {
+        icon.src = "/img/Lv4_stemp.png";
+        name.innerText = "현재 등급은 Lv.4 타조입니다"
+        remain = 50 - sum;
+    }else {
+        icon.src = "/img/Lv5_stemp.png";
+        name.innerText = "현재 등급은 Lv.5 치타입니다"
+    }
+    if(sum < 50) document.getElementsByClassName('to-next-level')[0].innerText = `다음 등급까지 ${remain}도움 남았습니다`;
+    else document.getElementsByClassName('to-next-level')[0].innerText = `당신은 멋진 사람입니다!`;
+}
 
 function backMyPage(){
     window.location.href = "/myPage.html";
