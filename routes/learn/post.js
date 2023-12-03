20c3817c83f4327a0a3f92cf5006eb72bec637a6
이서영 <88700475+seoyoung1276@users.noreply.github.com>
2023-12-03 22:09:52 +0900

    배우기 isfinish 추기

--- routes/learn/post.js
+++ routes/learn/post.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { createPost, getAllPost, getOnePost, getUserPost, updatePost, deletePost} = require('../../controllers/learn/post');
+const { createPost, getAllPost, getOnePost, getUserPost, updatePost, deletePost, isFinishPost} = require('../../controllers/learn/post');
 
 
 router.post('/', createPost);
@@ -14,5 +14,7 @@ router.get('/user/:userno', getUserPost)
 router.patch('/:postno', updatePost)
 
 router.delete('/:postno', deletePost)
+
+router.patch('/isfinish/:postno', isFinishPost)
 
 module.exports = router;
